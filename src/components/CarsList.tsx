9fd32d63066b1053947828438055d573bd16add7
ElvisMuresan <elvis.e.muresan@gmail.com>
2024-08-24 22:20:57 +0300

    feat: Designed the table

--- src/components/CarsList.tsx
+++ src/components/CarsList.tsx
@@ -47,26 +47,28 @@ const CarsList: React.FC<CarsListProps> = ({ token }) => {
   }
 
   return (
-    <div className="mt-8">
+    <div >
       <h2 className="text-2xl mb-4">Cars List</h2>
-      <table className="min-w-full">
+      <table className="mt-11 table-auto mb-4 w-full bg-slate-800 border-collapse border border-slate-400 rounded-md p-14 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative">
         <thead>
           <tr>
-            <th className="py-2">Brand</th>
-            <th className="py-2">Model</th>
-            <th className="py-2">Color</th>
-            <th className="py-2">Engine</th>
-            <th className="py-2">HorsePower</th>
+            <th className="text-right px-5 py-3 border-b">Id</th>
+            <th className="text-right px-5 py-3 border-b">Brand</th>
+            <th className="text-right px-5 py-3 border-b">Model</th>
+            <th className="text-right px-5 py-3 border-b">Color</th>
+            <th className="text-right px-5 py-3 border-b">Engine</th>
+            <th className="text-right px-5 py-3 border-b">HorsePower</th>
           </tr>
         </thead>
         <tbody>
           {cars.map(car => (
-            <tr key={car.id}>
-              <td className="border px-4 py-2">{car.brand}</td>
-              <td className="border px-4 py-2">{car.model}</td>
-              <td className="border px-4 py-2">{car.color}</td>
-              <td className="border px-4 py-2">{car.engine}</td>
-              <td className="border px-4 py-2">{car.horsePower}</td>
+            <tr className="cursor-pointer hover:bg-slate-600 transition-colors" key={car.id}>
+              <td className="text-center border px-4 py-2">{car.id}</td>
+              <td className="text-center border px-4 py-2">{car.brand}</td>
+              <td className="text-center border px-4 py-2">{car.model}</td>
+              <td className="text-center border px-4 py-2">{car.color}</td>
+              <td className="text-center border px-4 py-2">{car.engine}</td>
+              <td className="text-center border px-4 py-2">{car.horsePower}</td>
             </tr>
           ))}
         </tbody>
