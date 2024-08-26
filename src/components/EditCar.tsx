b2a527137f1c3a481a54ee3dd4eeabf9a7be3f35
ElvisMuresan <elvis.e.muresan@gmail.com>
2024-08-26 20:29:35 +0300

    feat: Designed the edit page

--- src/components/EditCar.tsx
+++ src/components/EditCar.tsx
@@ -44,74 +44,98 @@ const EditCar: React.FC<EditCarProps> = ({ token }) => {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-10">
-      <h2 className="text-2xl mb-4">Edit Car</h2>
-      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Brand
-          </label>
+    <div className='bg-slate-800 border border-slate-400 rounded-md p-24 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-30 relative'>
+      <h2 className='text-4xl text-white font-bold text-center mb-6'>Edit Car</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="relative my-4">
           <input
             type="text"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            className='peer block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-cyan-700'
+            placeholder=" "
             value={brand}
             onChange={(e) => setBrand(e.target.value)}
           />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Model
+          <label 
+            htmlFor="brand"               
+            className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+          >
+            Brand
           </label>
+        </div>
+
+        <div className="relative mb-4">
           <input
             type="text"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="model"
+            className='peer block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-cyan-700'
+            placeholder=" "
             value={model}
             onChange={(e) => setModel(e.target.value)}
           />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Color
+          <label  
+            htmlFor="model"
+            className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+          >
+            Model
           </label>
+        </div>
+
+        <div className="relative mb-4">
           <input
             type="text"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="color"
+            className='peer block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-cyan-700'
+            placeholder=" "
             value={color}
             onChange={(e) => setColor(e.target.value)}
           />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            Engine
+          <label  
+            htmlFor="color"
+            className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+          >
+            Color
           </label>
+        </div>
+
+        <div className="relative mb-4">
           <input
             type="text"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="engine"
+            className='peer block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-cyan-700'
+            placeholder=" "
             value={engine}
             onChange={(e) => setEngine(e.target.value)}
           />
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">
-            HorsePower
+          <label  
+            htmlFor="engine" 
+            className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
+          >
+            Engine
           </label>
+        </div>
+
+        <div className="relative mb-4">
           <input
             type="number"
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="horsePower"
+            className='peer block w-72 py-2.5 px-0 text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-cyan-700'
+            placeholder=" "
             value={horsePower}
             onChange={(e) => setHorsePower(parseInt(e.target.value, 10))}
           />
-        </div>
-        <div className="flex items-center justify-between">
-          <Button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          <label 
+            htmlFor="horsePower" 
+            className='absolute text-sm text-white duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-white peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6'
           >
-            Update Car
-          </Button>
+            HorsePower
+          </label>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <Button className='w-full mb-4 text-[18px] mt-6 rounded-full py-2' type="submit">Update Car</Button>
         </div>
       </form>
-    </div>
+  </div>
   );
 };
 
