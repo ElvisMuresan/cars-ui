576bd405bdb100cf78874d47d9b76bb47eb6a41e
ElvisMuresan <elvis.e.muresan@gmail.com>
2024-08-26 08:19:44 +0300

    feat: Moved the logout request in car-api file

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import axios from 'axios';
 import { Button } from 'flowbite-react';
 import { useNavigate } from 'react-router-dom';
+import { logoutUser } from './api/car-api';
 
 interface LogoutProps {
   token: string;
@@ -10,17 +10,10 @@ interface LogoutProps {
 
 const Logout: React.FC<LogoutProps> = ({ token, clearToken }) => {
   const navigate = useNavigate()
+
   const handleLogout = async () => {
     try {
-      await axios.post(
-        'http://localhost:3000/logout',
-        {},
-        {
-          headers: {
-            Authorization: token,
-          },
-        }
-      );
+      await logoutUser(token)
       clearToken();
       localStorage.removeItem('token');
       localStorage.removeItem('username');
--- src/components/api/car-api.ts
+++ src/components/api/car-api.ts
@@ -4,12 +4,27 @@ const BASE_URL = 'http://localhost:3000'
 
 export const loginUser = async (username: string, password: string) => {
     try {
-      const response = await axios.post('http://localhost:3000/login', {
+      const response = await axios.post(`${BASE_URL}/login`, {
         username,
         password,
       });
       return response.data;
     } catch (error) {
       throw new Error('Invalid credentials');
+    }
+  };
+
+export const logoutUser = async (token: string) => {
+    try {
+      const response = await axios.post(`${BASE_URL}/logout`, {
+      },
+    {
+        headers: {
+            Authorization: token,
+        },
+    });
+      return response.data;
+    } catch (error) {
+      throw new Error('Logout failed');
     }
   };
